c6c04d8d272e56978d002f03daa0d3b202500979
rrama19a <157783796+rrama19a@users.noreply.github.com>
2024-02-09 04:36:27 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,57 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Example'),
+      ),
+      body: Center(
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 8, 131, 47),
+              ),
+              child: Text(
+                'Информация',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Кайратулы Рамазан'),
+              onTap: () {
+              },
+            ),
+            ListTile(
+              title: Text('ra,ka@example.com'),
+              onTap: () {
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   @override
