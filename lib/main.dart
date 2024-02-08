388298595035a885ec515c052741c8703654a2d7
rrama19a <157783796+rrama19a@users.noreply.github.com>
2024-02-09 04:31:31 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,47 @@ class MyApp extends StatelessWidget {
 }
 
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Linear Progress Bar'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 50.0,
+                width: 50.0,
+                child: CircularProgressIndicator(),
+              ),
+              SizedBox(height: 20.0),
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: LinearProgressIndicator(),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Loading...',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
 
 class MyApp extends StatelessWidget {
   @override
